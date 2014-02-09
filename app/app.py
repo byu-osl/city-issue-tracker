e85e37db1664f11af762123301d043694e7e027a
David Barley <ghuitster@gmail.com>
2014-02-08 18:18:06 -0700

    Added a basic route for demonstration purposes.

--- app/app.py
+++ app/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('hello_world.html')
+	return render_template('hello_world.html')
+
+@app.route('/issue/<int:issue_num>', methods = ['GET', 'POST'])
+def view_issue(issue_num):
+	if request.method == 'GET':
+		return 'Stub for viewing issue %d' % issue_num
+	else:
+		return 'Trying to post to issue %d' % issue_num
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
