19fdb696115e4880c8ab3209bf495768beb031f1
David Barley <ghuitster@gmail.com>
2014-02-08 17:06:56 -0700

    Changed the app to use the rendered template.

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('hello_world.html')
 
 if __name__ == '__main__':
     app.run()
