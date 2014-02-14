e3d5e60a9fe513bbe94b7936dac921604b5dc9c8
Kamn <kamn.miller@gmail.com>
2014-02-13 21:39:06 -0700

    Some changes to clarify some variables

--- install_cit.py
+++ install_cit.py
@@ -1,3 +1,5 @@
+#NOTE: This is not done or tested and does NOT work on Windows.
+
 #TODO: Include the she-bang for python so it makes it easier to use
 
 #A simple install script to install city-issue-tracker
@@ -9,66 +11,58 @@ import os
 
 #Consts
 local = "local"
-globe = "global"
+system = "system"
 
-#
-path = '' #Normal the path is nothign except in the case of a local install
+path = '' #Normal the path is nothing except in the case of a local install(which is default)
 
 parser = argparse.ArgumentParser(description="Setup, Install and create run scripts for City-Issue-Tracker")
 
 #Add an argument that will do a global install
-parser.add_argument('--system_wide', action='store_const', const=globe, default=local, help="Enable global install")
+parser.add_argument('--system_wide', action='store_const', const=system, default=local, help="Enable system wide install")
 
 args = parser.parse_args();
 
 print(args)
 
 #If sing the local install
-if args.system_wide == "local":
+if args.system_wide == local:
 
 	#TODO Rename to pyenv_folder
-	tar_file = 'virtualenv-1.9'
+	pyenv_folder = 'virtualenv-1.9'
 	#Get the virtualenv
 	# http://www.virtualenv.org/en/latest/virtualenv.html#installation
-	call(['curl', '-0', 'https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.gz', '-o',tar_file+'.tar.gz'], stdout=subprocess.PIPE)
+	call(['curl', '-0', 'https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.gz', '-o', pyenv_folder+'.tar.gz'], stdout=subprocess.PIPE)
 
 	# untar it
-	call(['tar','xvfz', tar_file+'.tar.gz']);
+	call(['tar','xvfz', pyenv_folder+'.tar.gz']);
 
 	# delete tar file
-	call(['rm',tar_file+'.tar.gz'])
+	call(['rm',pyenv_folder+'.tar.gz'])
 
 	#Go into folder( I think this has to happen for the pip fallback stuff)
-	os.chdir(tar_file)
-
-	#Just to make sure we are in the correct dir
-	print(os.getcwd())
+	os.chdir(pyenv_folder)
 
 	# Activate the virtualenv
 	call(['python','virtualenv.py','CIT'])
 
-	#Just out a folder
+	#Back out of the folder
 	os.chdir('..')
 
 	#Setup the python path to use
-
-
-path = './virtualenv-1.9/CIT/bin/';
+	path = './virtualenv-1.9/CIT/bin/';
 
 print(path+'pip')
 
 #TODO: Install the depens
 call([path + 'pip','install','Flask'])
 
-
-f = open('run.py','w')
 #Create the run file
-print("#!"+path+"python\nfrom app import app\napp.run()",file=f)#Time travel used
+f = open('run.py','w')
+#NOTE: I don't want it done this way but for now it works
+print("#!"+path+"python\nfrom subprocess import Popen\nPopen(['"+path+"python"+"','app/app.py'])",file=f)#Time travel used
 f.close()
 
 #Make newly created file runnable
 call(['chmod','a+x','run.py'])
 
-
-
-#TODO: http://docs.python.org/2/distutils/setupscript.html <- Setup something like that
+#TODO: http://docs.python.org/2/distutils/setupscript.html <- Setup something like that(maybe?)
