8285edd61c9328e28259faa428a38df36847ec04
Kamn <kamn.miller@gmail.com>
2014-02-22 17:10:38 -0700

    Added a commented line to help with the DB connection in the future

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = "???"
 db = SQLAlchemy(app)
+
+
 
 from app import views
