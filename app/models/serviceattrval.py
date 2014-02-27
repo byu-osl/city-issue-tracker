96d1e20416ede8a6e17207e06900ce1a13a45417
Kamn <kamn.miller@gmail.com>
2014-02-26 21:24:32 -0700

    Added the ServiceAttributeValue model to help with singlevaluelist and multivaluelist

---
+++ app/models/serviceattrval.py
@@ -0,0 +1,17 @@
+from app import db
+
+class ServiceAttributeValue(db.Model):
+	"""
+	This model helps Service Attributes deal with the `singlevaluelist` and `multivaluelist` datatypes
+
+	Attributes
+	id (int): The primary key of the object
+	service_attr_id (string): The foreign key of the service to point too
+	name (string): The name of the key
+	
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	#TODO: should the key be the same as the ID?
+	service_attr_id = db.Column(db.Integer) #TODO: Make into foreign key
+	name = db.Column(db.String(100))
+
