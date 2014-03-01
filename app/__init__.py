5ecf13974b7abe5728628b85802ec872dd57e3ca
Kamn <kamn.miller@gmail.com>
2014-03-01 13:30:44 -0700

    Commit some code that uses sqlite for local testing

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = "???"
+
+#import os
+#basedir = os.path.abspath(os.path.dirname(__file__))
+#For local testing reasons
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
 db = SQLAlchemy(app)
 
-
+#db.create_all()
 
 from app import views
