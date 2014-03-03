188517d39fd641d8527cad7bc01e5eab24c6d51b
CameronEzraJones <cejones90@msn.com>
2014-03-03 11:50:42 -0700

    added my test file

---
+++ app/cameron_testing.py
@@ -0,0 +1,26 @@
+from flask.ext.testing import Testing
+from app import app, db
+
+class MyTest(TestCase):
+
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+    TESTING = True
+
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        return app
+    
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        
+class SubTestServices(MyTest):
+    
+    def test_something(self):
+        pass
+    
+    pass
