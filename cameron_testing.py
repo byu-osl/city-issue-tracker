003cabc38d53c6636d1a07d69553b80e0635809e
CameronEzraJones <cejones90@msn.com>
2014-03-13 13:11:11 -0600

    Added more test cases

--- cameron_testing.py
+++ cameron_testing.py
@@ -39,19 +39,68 @@ class SubTest_Services(MyTest):
         db.session.add(k)
         k1 = models.Keyword()
         db.session.add(k1)
-        s = models.Service(id=1, service_name='Take out trash', description='Take out trash', meta_data=True, type='realtime', attributes=[sa, sa1], keywords=[k, k1])
+        s = models.Service(
+            serviceId=1,
+            serviceName='Take out trash',
+            description='Take out trash',
+            metaData=True,
+            type='realtime',
+            attributes=[sa, sa1],
+            keywords=[k, k1])
         db.session.add(s)
         db.session.commit()
         assert s in db.session
         
-        s1 = models.Service.query.filter_by(service_name='Take out trash').first()
-        assert s1.id == 1
-        assert s1.service_name == 'Take out trash'
+        s1 = models.Service.query.filter_by(serviceName='Take out trash').first()
+        assert s1.serviceId == 1
+        assert s1.serviceName == 'Take out trash'
         assert s1.description == 'Take out trash'
         
         db.session.delete(s1)
         db.session.commit()
         assert s1 not in db.session
+
+    def test_Two_Services(self):
+        print '\ntest_Two_Services'
+        sa = models.ServiceAttribute()
+        db.session.add(sa)
+        sa1 = models.ServiceAttribute()
+        db.session.add(sa1)
+        k = models.Keyword()
+        db.session.add(k)
+        k1 = models.Keyword()
+        db.session.add(k1)
+        s1 = models.Service(
+            serviceId=1,
+            serviceName='Repair sidewalk',
+            description='Chipped sidewalk needs repair',
+            metaData=True,
+            type='realtime',
+            attributes=[sa1],
+            keywords=[k,k1])
+        s2 = models.Service(
+            serviceId=2,
+            serviceName='Take out trash',
+            description='Overflowing garbage bin',
+            metaData=True,
+            type='batch',
+            attributes=[sa,sa1],
+            keywords=[k])
+        db.session.commit()
+        assert s1 in db.session
+        assert s2 in db.session
+
+        s3 = models.Service.query.filter_by(type='batch').first()
+        assert s3.serviceId == 2
+
+        db.session.delete(s1)
+        db.session.commit()
+        assert s1 not in db.session
+        db.session.delete(s2)
+        db.session.commit()
+        assert s2 not in db.session
+
+class SubTestUser(MyTest):
     
     def test_User(self):
         print '\ntest_User'
@@ -59,7 +108,6 @@ class SubTest_Services(MyTest):
         db.session.add(sr)
         user = models.User(
             userId=1,
-            email='cejonesmsncom',
             firstName='Cameron',
             lastName='Jones',
             phone='7192007970',
@@ -88,8 +136,36 @@ class SubTest_Services(MyTest):
         db.session.commit()
         assert user not in db.session
 
-def SubTestUser(MyTest):
-    pass
+    def test_Two_Users(self):
+        print '\ntest_Two_Users'
+        user = models.User(
+            userId=1,
+            firstName='Cameron',
+            lastName='Jones',
+            phone='7192007970',
+            passwordHash='60744474c3277428a8be861186e1e368',
+            passwordSalt='s0mRIdlKvI',
+            role='user',
+            lastLogin=date.today(),
+            joined=date.today())
+        user1 = models.User(
+            userId=1,
+            firstName='Susan',
+            lastName='Jones',
+            phone='7194602421',
+            role='employee')
+        db.session.add(user)
+        db.session.add(user1)
+        from sqlalchemy.exc import IntegrityError
+        self.assertRaises(IntegrityError, lambda: db.session.commit())
+        assert user1 not in db.session
+
+class SubTestAgency(MyTest):
+    
+    def test_Agency(self):
+        print '\ntest_Agency'
+        
+        pass
 
 if __name__ == '__main__':
     unittest.main()
