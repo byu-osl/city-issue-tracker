267120c75a79b8f0cb348f70b565d45aaca7e39e
David Barley <ghuitster@gmail.com>
2014-03-14 22:40:32 -0600

    Finishing up the routes from the wiki page Kyle posted. Made a few minor cosmetic changes to that file. Deleted a bunch of the templates we won't be using.

--- app/views.py
+++ app/views.py
@@ -3,15 +3,19 @@ from flask import render_template, request, jsonify
 from fakeData import service_list, service_def, get_service_reqs, get_service_req, user_data
 from models import Service, ServiceAttribute, Keyword, KeywordMapping
 
-@app.route('/hello_world')
-def hello_world():
-	return render_template('hello_world.html')
+#############
+# Main Page #
+#############
 
 #TODO: Make sure the html is being served up correctly
 @app.route('/index')
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+################
+# User Section #
+################
 
 #TODO: How to do this securely
 @app.route('/users/sign_in', methods=['POST'])
@@ -25,90 +29,92 @@ def signOut():
 
 #TODO: Implement
 @app.route('/users', methods=['POST'])
-def newUsers():
+def createUser():
 	return "---"
 
-
-#TODO: Implements
-@app.route('/users/<int:user_id>')
+#TODO: Implement
+@app.route('/users/<int:user_id>', methods=['GET'])
 def getUser(user_id):
 	return "---"
 
-#TODO: Implements
+#TODO: Implement
+@app.route('/users/<int:user_id>', methods=['POST'])
+def updateUser(user_id):
+	return "---"
+
+#TODO: Implement
+@app.route('/users/signed_in_user')
+def getOwnAccount():
+	return "---"
+
+#TODO: Implement
 @app.route('/users', methods=['GET'])
 def getAllUsers():
 	return "---"
 
-#TODO: 
+##################
+# Issues Section #
+##################
+
+#TODO: Implement
 @app.route('/issues/<int:issue_id>', methods=['GET'])
 def getIssue():
 	return "---"
 
-#TODO: 
+#TODO: Implement
+@app.route('/issues', methods=['POST'])
+def createIssue():
+	return "---"
+
+#TODO: Implement
 @app.route('/issues/<int:issue_id>', methods=['POST'])
 def updateIssue():
 	return "---"
 
-
-
-
-
-#TODO: 
+#TODO: Implement
 @app.route('/issues', methods=['GET'])
-def newIssue():
+def viewAllIssues():
 	return "---"
 
+#TODO: Implement
+@app.route('/issues/images', methods=['POST'])
+def uploadImage():
+	return "---"
 
+#TODO: Implement
+@app.route('/issues/images/<int:photo_id>', methods=['GET'])
+def viewImage(photo_id):
+	return "---"
 
+##############
+# API Routes #
+##############
 
-
-
-
-
+#TODO: Implement
 @app.route('/api/services', methods=['GET'])
 def json_view_services():
 	return jsonify(service_list)
 
-
+#TODO: Implement
 @app.route('/api/issue/<int:issue_id>', methods = ['GET'])
 def json_view_issue(issue_id):
 	return jsonify(get_service_req)
 
+#TODO: Implement
 @app.route('/api/issues', methods = ['GET'])
 def json_view_issues():
 	return jsonify(get_service_reqs)
 
-@app.route('/issue/<int:issue_num>', methods = ['GET', 'POST'])
-def view_issue(issue_num):
-	if request.method == 'GET':
-		return render_template('view_issue.html', issue_num = issue_num, issue = get_service_req)
-	elif request.method == 'POST':
-		return render_template('post_issue.html', issue_num = issue_num, issue = get_service_req)
+##################
+# Error Handlers #
+##################
 
-@app.route('/login')
-def login():
-	return render_template('login.html')
-
-@app.route('/logout')
-def logout():
-	return render_template('logout.html')
-
-@app.route('/issues')
-def issues():
-	return render_template('issues.html', issues = get_service_reqs)
-
-@app.route('/admin')
-def admin():
-	return render_template('admin.html')
-
-@app.route('/profile')
-def profile():
-	return render_template('profile.html', user_data = user_data)
-
+#TODO: Implement
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
 
+#TODO: Implement
 @app.errorhandler(405)
 def page_not_found(e):
 	return render_template('405.html'), 405
