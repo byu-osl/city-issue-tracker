165b2c3b3f23d414ae000a485cbdd6dd008f24ca
Kamn <kamn.miller@gmail.com>
2014-03-29 14:35:30 -0600

    Added some simple docs to the genError and ValidatioError

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 
-class ValidationError(Exception):
-	def __init__(self, str):
-		self.str = str
-	def __str__(self):
-		return repr(self.str)
 
+#TODO: Move Validation Error elsewhere?
+class ValidationError(Exception):
+	"""
+	A simple Validation Error class for dealing with problems when models 
+	Have invalid data that is set
+	"""
+	def __init__(self, msg):
+		self.errorMsg = msg
+	def __str__(self):
+		return repr(self.errorMsg)
+
+#TODO: Move genError elsewhere?
 def genError(code, desc):
+	"""
+	A simple error code generate modeled after Open311's error codes
+	"""
 	jsonResp = jsonify({
 		"code": code,
 		"description": desc
@@ -18,7 +28,14 @@ def genError(code, desc):
 	return jsonResp;
 
 
+#Image upload settings
+UPLOAD_FOLDER = 'data/img/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
